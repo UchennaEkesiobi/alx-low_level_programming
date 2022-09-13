2249d05f1b32e10cae89a6f2451b92b803c2f199
UchennaEkesiobi <uche.ekesiobi@gmail.com>
2022-09-13 13:01:15 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,13 @@
 #include "main.h"
 /**
  * _islower - function to check for uppercase character
- * @c: is the int that will use for the argument of the function
- *
+ * @c: is the int that we use for the argument of the function
  * Return: 0
  */
 int _islower(int c)
 
 {
-	if (c = 'a'; && c <= 'z')
+	if (c = 'a'; && c <= 'z');
 	{
 		return (1);
 	}
