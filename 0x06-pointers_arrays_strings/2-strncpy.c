299371b7dfaa0ee36f7e58e56af9e0b0245427c0
UchennaEkesiobi <uche.ekesiobi@gmail.com>
2022-09-21 17:41:01 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strncpy - c function that copies a string
+ *
+ * Return: returns
+ */
+
+char *_strncpy(char *dest, char *src, int n);
+{
+	int = i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for (i = 0; i < n; i++)
+		dest[i] = '\0';
+	return (dest);
+}
