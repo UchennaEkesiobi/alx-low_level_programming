bd5539bf4156e897df05a33c2a7697ab092cc047
UchennaEkesiobi <uche.ekesiobi@gmail.com>
2022-09-21 17:46:57 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,6 +9,7 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
+
 	int index = 0, dest_len = 0;
 
 	while (dest[index++])
@@ -16,5 +17,7 @@ char *_strncat(char *dest, char *src, int n)
 
 	for (index = 0; src[index] && index < n; index++)
 		dest[dest_len++] = src[index];
+
 	return (dest);
+
 }
