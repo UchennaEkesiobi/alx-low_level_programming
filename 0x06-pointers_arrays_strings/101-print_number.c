8f126754c5da3d0319813f415e198b41e89cf2b3
UchennaEkesiobi <uche.ekesiobi@gmail.com>
2022-09-22 20:10:40 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -12,12 +12,12 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		_putchar('_');
+		putchar('_');
 		n *= -1;
 	}
 	x = n;
 
 	if (x / 10)
 		print_number(x / 10);
-	_putchar(x % 10 + '0');
+	putchar(x % 10 + '0');
 }
