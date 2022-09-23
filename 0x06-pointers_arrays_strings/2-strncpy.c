c1891c7f6fe3ed8924128051d419f157dc9c12b1
UchennaEkesiobi <uche.ekesiobi@gmail.com>
2022-09-22 17:08:36 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -7,11 +7,24 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int = i;
+	int a = 0, b = 0;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
-	for (i = 0; i < n; i++)
-		dest[i] = '\0';
+	while (src[b])
+	{
+		b++;
+	}
+
+	while (a < n && src[a])
+	{
+		dest[a] = src[a];
+		a++;
+	}
+
+	while (a < n)
+	{
+		dest[a] = '\0';
+		a++;
+	}
+
 	return (dest);
 }
