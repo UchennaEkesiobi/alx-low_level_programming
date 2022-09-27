bb0621801dd72c0ed57788e4557c0214ec8bc3a3
UchennaEkesiobi <uche.ekesiobi@gmail.com>
2022-09-27 02:37:05 -0700

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystach: string in which to check for needle
+ * @needle: substring to find a haystack
+ *
+ * Return: pointer to beginning of needle in haystack of NULL if no match
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i = 0, j = 0;
+
+	while (haystack[i])
+	{
+		while ( needle[j] && (haystack[i] == needle[0]))
+		{
+			if (haystack [i + j] == needle[j])
+				j++;
+			else
+				break;
+		}
+		if (needle[j])
+		{
+			i++;
+			j = 0;
+		}
+		else
+			return (haystack + 1);
+	}
+	return (0);
+}
